1d739f614f50e287f9abb7e82c9f1e6a0305e07d
vladsemakin49 <vladsemakin49@gmail.com>
2026-02-13 22:28:55 +0300

    Добавляет команду для отрисовки пораболы с помощью псгрф

--- plot.py
+++ plot.py
@@ -3,14 +3,14 @@ def print_plot_1d(function, x_min, x_max, point_count):
     for i in range (point_count):
         x = x_min + dx * i
         y = function(x)
-        point = '' * int(y) + '.'
-        print(count)
+        point = ' ' * int(y) + '▪️'
+        print (point)
 
 def f(x):
-    return x * x_max
-min = 0
-max = 10
+    return x
+
+x_min = 0
+x_max = 10
 points = 10
 
-
-print_plot_1d(f,min,max,points)
+print_plot_1d(f,x_min,x_max,points)
